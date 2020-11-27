94db973dfd0d9a81a21b70c663139f89074cea10
xlgabriel <gabriel.jeannot@uao.edu.co>
2020-11-27 10:54:50 -0500

    AdelantoViernes
    Incluye detalles del futuro y ejecutable.

--- Escenarios/Futuro/Control/Scripts/FinishLevel.cs
+++ Escenarios/Futuro/Control/Scripts/FinishLevel.cs
@@ -8,12 +8,21 @@ public class FinishLevel : MonoBehaviour
     public GameObject levelMusic;
     public AudioSource levelComplete;
     public GameObject levelTimer;
+    public GameObject CuboAparecer;
+    public GameObject PanelVictoria;
+    public GameObject Jugador;
+    public GameObject CamaraVictoria;
 
     void OnTriggerEnter()
     {
+        CuboAparecer.SetActive(true);
         levelMusic.SetActive(false);
         levelTimer.SetActive(false);
         levelComplete.Play();
+        PanelVictoria.SetActive(true);
+        CamaraVictoria.SetActive(true);
+        Jugador.SetActive(false);
+       
     }
 
 }
